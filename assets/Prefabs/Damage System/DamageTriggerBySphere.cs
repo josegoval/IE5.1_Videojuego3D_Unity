a0617522cd30e616c7acd4849f4d4d686721d24b
josegoval <56361627+josegoval@users.noreply.github.com>
2021-02-22 15:04:49 +0100

    DamageTriggerBySphere setup

---
+++ assets/Prefabs/Damage System/DamageTriggerBySphere.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTriggerBySphere : MonoBehaviour
+{
+    public float damagePoints = 2f;
+    public float sphereRadius = 1f;
+    public LayerMask layerMaskToAttack;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, sphereRadius, layerMaskToAttack);
+
+        if (colliders.Length > 0)
+        {
+            print("HIT: " + colliders[0].gameObject.tag);
+
+            gameObject.SetActive(false);
+        }
+    }
+}
